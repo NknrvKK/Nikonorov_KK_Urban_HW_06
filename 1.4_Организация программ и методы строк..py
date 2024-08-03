c1beb51bf7861240b2c012e0dd056c66d1ba2e89
NknrvKK <167916590+NknrvKK@users.noreply.github.com>
2024-08-03 12:28:12 +0300

    Add files via upload

---
+++ 1.4_Организация программ и методы строк..py
@@ -0,0 +1,9 @@
+my_string = input("Вводи текст: ") # Привет, UrBaNist! :), КаК деЛА?
+print(len(my_string))
+print(my_string.upper()) # в верхнем регистре. #
+print(my_string.lower()) # в нижнем регистре. #
+print(my_string.replace(" ", "")) # удалив все пробелы. Метод replace - сначала пишем,
+# что хотим заменить, А ПОСЛЕ пишем, на что заменяем
+#
+print(my_string[0]) # в верхнем регистре. #
+print(my_string[-1]) # в верхнем регистре. #
